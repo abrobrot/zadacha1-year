730b0ccd61b8c1e6a1bd36df57529225ae8aacb4
abrobrot <olegstakhov210@gmail.com>
2025-02-28 12:40:15 +0300

    Add files via upload

---
+++ zadacha 2 bank.py
@@ -0,0 +1,9 @@
+def bank (year,a):
+    i=0
+    while i < year:
+        a = a*1.1
+        i +=1
+    print(a)
+year=(int(input('Введите срок в годах: ')))
+a=(int(input('Введите сумму: ')))
+bank(year,a)
